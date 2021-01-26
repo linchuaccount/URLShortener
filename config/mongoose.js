1d3c86e43a75050a7624d662d8cb7029aa338c60
linchuaccount <younchu123@gmail.com>
2021-01-26 18:38:08 +0800

    fix: revise MONGODB_URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 // 如果在 Heroku 環境則使用 process.env.MONGODB_URI
 // 否則為本地環境，使用 mongodb://localhost/URL-shortener
-const MONOGODB_URI = process.env.MONOGODB_URI || 'mongodb://localhost/URL-shortener'
-mongoose.connect(MONOGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/URL-shortener'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
